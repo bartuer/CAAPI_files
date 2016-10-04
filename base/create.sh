49482a02a3e3cfe0830664f6221a4846d4dcc5c0
bartuer zhou <bazhou@microsoft.com>
2016-10-04 19:21:55 +0800

    vms permission

--- base/create.sh
+++ base/create.sh
@@ -2,5 +2,6 @@
 sudo mkdir -p /ml/{local,storage} && sudo chmod -R g+w /ml
 cd /ml
 sudo git clone https://github.com/bartuer/CAAPI_files.git vms
+sudo chown -R $(whoami):root /ml/vms
 cd /ml/vms/base
 vagrant reload
