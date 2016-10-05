6be1274b141b3452d9f9b9096e2d877852ec8e01
Bartuer Zhou <bazhou@microsoft.com>
2016-10-05 04:14:06 +0000

    ignore and minimal

---
+++ minimal/create.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo mkdir -p /ml/{local,storage} && sudo chmod -R g+w /ml
+cd /ml
+sudo git clone https://github.com/bartuer/CAAPI_files.git vms
+sudo chown -R $(whoami):root /ml/vms
+cd /ml/vms/minimal
+vagrant up
