041b8e85e771463f2e890fd696f6e077d4a98521
Bartuer Zhou <bazhou@microsoft.com>
2016-10-08 03:25:57 +0000

    remove base

--- base/create.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo mkdir -p /ml/{local,storage} && sudo chmod -R g+w /ml
-cd /ml
-sudo git clone https://github.com/bartuer/CAAPI_files.git vms
-sudo chown -R $(whoami):root /ml/vms
-cd /ml/vms/base
-vagrant up
